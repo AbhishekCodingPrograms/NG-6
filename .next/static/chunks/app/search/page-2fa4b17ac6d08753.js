6fd8e98bbdeb1e79fcd6009eefc799b58fc1c38d
Abhishek <abhishek@notesgallery.in>
2026-06-07 04:54:47 +0530

    Remove directUrl from prisma.config to fix Vercel build

---
+++ .next/static/chunks/app/search/page-2fa4b17ac6d08753.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[797,42,91,404,312,275,882,109,95,931,596,301],{3521:function(){}},function(n){n.O(0,[971,997,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
